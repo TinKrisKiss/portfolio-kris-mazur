17584d608d99f68cfb532ed10d49bc1030cb2829
TinKrisKiss <z-qksi@yandex.ru>
2026-07-16 17:44:17 +0500

    осстановление связи с GitHub и синхронизация файлов

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#1e1e1e',
+        light: '#ffffff',
+        'accent-cyan': '#00f0ff',
+        'accent-coral': '#ff6b6b',
+        muted: '#6c6c6c'
+      },
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+        heading: ['Tenor Sans', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
